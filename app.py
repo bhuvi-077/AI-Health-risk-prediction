745b1435ec2cf9611288974c91cd0714c2a79662
Bhuvi <yuvis1780@gmail.com>
2026-05-17 09:14:58 +0530

    UI modified

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 import joblib
 import numpy as np
 import base64
+import pandas as pd
+
+df = pd.read_csv("dataset/heart.csv")
 
 
 def set_bg():
-    with open("bg1.jpg", "rb") as file:
+    with open("bg.jpg", "rb") as file:
         encoded = base64.b64encode(file.read()).decode()
     page_bg = f"""
     <style>
@@ -22,7 +25,7 @@ set_bg()
 model = joblib.load("models/health_model.pkl")
 scaler = joblib.load("models/scaler.pkl")
 st.markdown(
-    "<h1 style = 'color:Black;'>AI Health Risk Prediction</h1>",unsafe_allow_html=True
+    "<h1 style = 'color:White;'>AI Health Risk Prediction</h1>",unsafe_allow_html=True
 )
 #st.title("AI Health Risk Prediction")
 
@@ -30,52 +33,76 @@ st.markdown(
 #bp = st.number_input("Blood Pressure", min_value = 0, max_value = 200, value = 120)
 #chol = st.number_input("cholestrol", min_value = 0, max_value = 500, value = 200)
 #st.sidebar.title("patient Details")
-col1,col2 = st.columns(2)
-with col1:
-    age = st.number_input("Age")
-    sex = st.selectbox("Sex", [0,1])
-    cp = st.number_input("Chest Pain Type")
+tab1, tab2 = st.tabs(["Prediction", "Analytics"])
 
-    trestbps = st.number_input("Resting Blood Pressure")
+with tab1:
+    col1,col2 = st.columns(2)
+    with col1:
+        age = st.number_input("Age")
+        sex = st.selectbox("Sex", [0,1])
+        cp = st.number_input("Chest Pain Type")
 
-    chol = st.number_input("Cholesterol")
+        trestbps = st.number_input("Resting Blood Pressure")
 
-    fbs = st.selectbox("Fasting Blood Sugar", [0,1])
+        chol = st.number_input("Cholesterol")
 
-    restecg = st.number_input("Rest ECG")
-with col2:
-    thalach = st.number_input("Max Heart Rate")
+        fbs = st.selectbox("Fasting Blood Sugar", [0,1])
 
-    exang = st.selectbox("Exercise Angina", [0,1])
+        restecg = st.number_input("Rest ECG")
+    with col2:
+        thalach = st.number_input("Max Heart Rate")
 
-    oldpeak = st.number_input("Oldpeak")
+        exang = st.selectbox("Exercise Angina", [0,1])
 
-    slope = st.number_input("Slope")
+        oldpeak = st.number_input("Oldpeak")
 
-    ca = st.number_input("CA")
+        slope = st.number_input("Slope")
 
-    thal = st.number_input("Thal")
-data = [[
-    age,
-    sex,
-    cp,
-    trestbps,
-    chol,
-    fbs,
-    restecg,
-    thalach,
-    exang,
-    oldpeak,
-    slope,
-    ca,
-    thal
-]]
-if st.button("predict"):
-    #data = model.transform(data)
-    #st.balloons()
-    prediction = model.predict(data)
-    if prediction[0] == 0:
-        st.error("High Health Risk")
-    else:
-        st.success("Low Health Risk")
-        st.balloons()
+        ca = st.number_input("CA")
+
+        thal = st.number_input("Thal")
+    data = [[
+        age,
+        sex,
+        cp,
+        trestbps,
+        chol,
+        fbs,
+        restecg,
+        thalach,
+        exang,
+        oldpeak,
+        slope,
+        ca,
+        thal
+    ]]
+
+    if st.button("predict"):
+        #data = model.transform(data)
+        #st.balloons()
+        with st.spinner("Analyzing patient data..."):
+            prediction = model.predict(data)
+        col1,col2 = st.columns(2)
+        with col1:
+            st.metric("Age", age)
+            st.metric("Blood pressure", trestbps)
+            st.metric("Cholesterol", chol)
+        if prediction[0] == 0:
+            st.error("High Health Risk")
+            with col2:
+                st.image("high risk.jpg", width = 400)
+        else:
+            st.success("Good Health")
+            with col2:
+                st.image("good health.jpg", width = 400)
+            st.balloons()
+        st.subheader("cholestrol Analysis")
+        st.bar_chart(df["chol"])
+with tab2:
+    st.header("Dataset Analytics")
+
+    st.line_chart(df["chol"])
+
+    with st.expander("Dataset Info"):
+
+        st.write(df.describe())
